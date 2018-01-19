34f6d0457eaada0951fd5b11eb73907ccbc2b359
Matthew Combatti <mcombatti@xojostudio.org>
2018-01-19 16:28:48 -0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "104.223.87.143:17662",
-  "104.223.87.127:17662",
+  "167.160.185.101:17662",
+  //"104.223.87.127:17662",
 };
 
 struct CheckpointData {
